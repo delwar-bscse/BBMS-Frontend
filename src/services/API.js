50c594721ffeb25170ccbd95c54089e739dff0e8
Md. Delwar Hossain <delwarbscse@gmail.com>
2024-09-12 03:56:17 +0600

    update API

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
+// const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
+const API = axios.create({ baseURL: 'https://bbms-backend.onrender.com/api/v1' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
